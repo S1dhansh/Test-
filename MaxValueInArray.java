8e659b39d9b1ce36c756ece510ae829278b1d808
S1dhansh <sidhanshsharma0@gmail.com>
2024-02-15 14:00:02 +0530

    This for maximum number in an index

--- MaxValueInArray.java
+++ MaxValueInArray.java
@@ -1,3 +1,18 @@
+import java.util.*;
 public class MaxValueInArray {
-
+    public static int findMax(int num[]){
+        int maxNum=Integer.MIN_VALUE;
+        for(int i =0;i<num.length;i++){
+            if (maxNum < num[i]) {
+                maxNum=num[i];
+            }
+        }
+        return maxNum;
+    }
+    public static void main(String args []){
+          int arr []={5,10,20,15};
+          int result= findMax(arr);
+          for(int i=0;i< arr.length;i++ ){}
+        System.out.println(result);
+      }
 }
